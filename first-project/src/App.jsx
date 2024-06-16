e7b624e3c832814283052a280732955fe33abf90
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-06-16 17:48:54 +0600

    practice

--- first-project/src/App.jsx
+++ first-project/src/App.jsx
@@ -4,6 +4,7 @@ import Header from "./components/Header";
 import Hero from "./components/Hero";
 
 const App = () => {
+  let marks=90;
   return (
     <div>
       <Header/>
@@ -11,7 +12,11 @@ const App = () => {
       <ContactForm/>
       <Footer/>
       <h1>{new Date().getFullYear()}</h1>
-      
+      { 
+        marks>95?
+        <h1>Brialiant Result</h1>:
+        <h1>Average Result</h1>
+      }
     </div>
   );
 };
