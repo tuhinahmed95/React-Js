eebafe54f4f83e161577fc5e4d5135428b5055b3
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-06-16 20:51:23 +0600

    update

--- first-project/src/App.jsx
+++ first-project/src/App.jsx
@@ -2,21 +2,17 @@ import ContactForm from "./components/ContactForm";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Result from "./components/Result";
 
 const App = () => {
-  let marks=90;
+  
   return (
     <div>
       <Header/>
       <Hero/>
       <ContactForm/>
       <Footer/>
-      <h1>{new Date().getFullYear()}</h1>
-      { 
-        marks>95?
-        <h1>Brialiant Result</h1>:
-        <h1>Average Result</h1>
-      }
+      <Result/>
     </div>
   );
 };
---
+++ first-project/src/components/Result.jsx
@@ -0,0 +1,36 @@
+
+
+const Result = () => {
+    let marks = 100;
+    return (
+        <div>
+            {(()=>{ 
+
+                if(marks>=80 && marks<100){ 
+                    return <h1>A+</h1>
+                }
+                else if(marks>=70 && marks<80){ 
+                    return <h1>A</h1>
+                }
+                else if(marks>=60 && marks<70){ 
+                    return <h1>A-</h1>
+                }
+                else if(marks>=50 && marks<60){ 
+                    return <h1>B</h1>
+                }
+                else if(marks>=40 && marks<50){ 
+                    return <h>C</h>
+                }
+                else if(marks>=33 & marks<40){ 
+                    return <h>D</h>
+                }
+                else{ 
+                    return <h1>Fail</h1>
+                }
+
+            })()}
+        </div>
+    );
+};
+
+export default Result;
