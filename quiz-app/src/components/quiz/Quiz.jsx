83b63b01a0039406a7322a8559435862a8e55ef3
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-06-22 11:59:41 +0600

    add js

--- quiz-app/src/components/quiz/Quiz.jsx
+++ quiz-app/src/components/quiz/Quiz.jsx
@@ -4,20 +4,30 @@ import { data } from './../../assets/data';
 
 const Quiz = () => {
 
-    let [index, setIndes] = useState(0);
+    let [index, setIndes] = useState(1);
     let [question, setQuestion] = useState(data[index])
+
+    const checkAns = (e,ans)=>{ 
+        if(question.ans===ans){ 
+            e.target.classList.add("correct");
+        }
+        else{ 
+            e.target.classList.add("wrong");
+
+        }
+    }
 
 
   return (
     <div className='container'>
         <h1>Quiz App</h1>
         <hr />
-        <h2>1. Which Device Is Required For The Internet Connection?</h2>
+        <h2>{index+1}. {question.question}</h2>
         <ul> 
-            <li>Modem</li>
-            <li>Router</li>
-            <li>Lab Cable</li>
-            <li>Pen Drive</li>
+            <li onClick={(e)=>{checkAns(e,1)}}>{question.option1}</li>
+            <li onClick={(e)=>{checkAns(e,2)}}>{question.option2}</li>
+            <li onClick={(e)=>{checkAns(e,3)}}>{question.option3}</li>
+            <li onClick={(e)=>{checkAns(e,4)}}>{question.option4}</li>
         </ul>
         <button>Next</button>
         <div className="index">1 of 5 questions</div>
