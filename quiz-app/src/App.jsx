5ab65013a1ddf04475c3eef6b1b0e91e94f703b1
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-06-22 10:59:30 +0600

    quiz start

--- quiz-app/src/App.jsx
+++ quiz-app/src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Quiz from './components/quiz/Quiz'
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <Quiz/>
+    </>
   )
 }
 
--- quiz-app/src/components/quiz/Quiz.jsx
+++ quiz-app/src/components/quiz/Quiz.jsx
@@ -3,7 +3,17 @@ import './Quiz.css'
 
 const Quiz = () => {
   return (
-    <div>
+    <div className='container'>
+        <h1>Quiz App</h1>
+        <hr />
+        <h2>Which Device Is Required For The Internet Connection?</h2>
+        <ul> 
+            <li>Modem</li>
+            <li>Router</li>
+            <li>Lab Cable</li>
+            <li>Pen Drive</li>
+        </ul>
+        <button>Next</button>
       
     </div>
   )
