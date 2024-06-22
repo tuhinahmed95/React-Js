08fda3ffc5500e943443753469c7636524a7b610
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-06-22 12:16:29 +0600

    update

--- quiz-app/src/components/quiz/Quiz.jsx
+++ quiz-app/src/components/quiz/Quiz.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import './Quiz.css'
 import { data } from './../../assets/data';
 
@@ -6,15 +6,29 @@ const Quiz = () => {
 
     let [index, setIndes] = useState(1);
     let [question, setQuestion] = useState(data[index])
+    let [lock, setLock] = useState(false);
+
+    let Option1 = useRef(null);
+    let Option2 = useRef(null);
+    let Option3 = useRef(null);
+    let Option4 = useRef(null);
+
+    let option_array = [Option1,Option2,Option3,Option4];
 
     const checkAns = (e,ans)=>{ 
-        if(question.ans===ans){ 
-            e.target.classList.add("correct");
+        if(lock===false){ 
+            if(question.ans===ans){ 
+                e.target.classList.add("correct");
+                setLock(true)
+            }
+            else{ 
+                e.target.classList.add("wrong");
+                setLock(true)
+                option_array[question.ans-1].current.classList.add("correct");
+    
+            }
         }
-        else{ 
-            e.target.classList.add("wrong");
-
-        }
+       
     }
 
 
@@ -24,10 +38,10 @@ const Quiz = () => {
         <hr />
         <h2>{index+1}. {question.question}</h2>
         <ul> 
-            <li onClick={(e)=>{checkAns(e,1)}}>{question.option1}</li>
-            <li onClick={(e)=>{checkAns(e,2)}}>{question.option2}</li>
-            <li onClick={(e)=>{checkAns(e,3)}}>{question.option3}</li>
-            <li onClick={(e)=>{checkAns(e,4)}}>{question.option4}</li>
+            <li ref={Option1}  onClick={(e)=>{checkAns(e,1)}}>{question.option1}</li>
+            <li ref={Option2}  onClick={(e)=>{checkAns(e,2)}}>{question.option2}</li>
+            <li ref={Option3} onClick={(e)=>{checkAns(e,3)}}>{question.option3}</li>
+            <li ref={Option4} onClick={(e)=>{checkAns(e,4)}}>{question.option4}</li>
         </ul>
         <button>Next</button>
         <div className="index">1 of 5 questions</div>
