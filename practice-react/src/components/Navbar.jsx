bc72c4676db46d3e11cb1251afe523047e3c6fd4
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-06-25 22:49:52 +0600

    practice

--- practice-react/src/components/Navbar.jsx
+++ practice-react/src/components/Navbar.jsx
@@ -3,14 +3,7 @@ import React from 'react';
 const Navbar = () => {
     return (
         <div>
-            <nav className='nav d-flex w-100 bg-primary'> 
-                <ul className='nav-item align-items-center'> 
-                    <li className='nav-link text-light'>Home</li>
-                    <li className='nav-link text-light'>about</li>
-                    <li className='nav-link text-light'>service</li>
-                </ul>
-                <input type="text" placeholder='search'/>
-            </nav>
+           
         </div>
     );
 };
