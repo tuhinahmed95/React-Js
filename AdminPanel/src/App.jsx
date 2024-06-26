b378e477baa252c7c83be40552e601efac077a55
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-06-26 23:09:58 +0600

    update

--- AdminPanel/src/App.jsx
+++ AdminPanel/src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from 'react'
 import './App.css'
+import Header from './Header'
+import Sidebar from './Sidebar'
+import Home from './Home'
 
 function App() {
 
 
   return (
     <div className='grid-container'> 
-
+      <Header />
+      <Sidebar />
+      <Home />
     </div>
   )
 }
--- AdminPanel/src/Header.jsx
+++ AdminPanel/src/Header.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
-
+import {BsFillBellFill, BsFillEnvelopeFill, BsPersonCircle, BsSearch, BsJustify} from 'react-icons/bs'
 function Header() {
   return (
-    <div>
-      
-    </div>
+    <header className='header'>
+        <div className='menu-icon'> 
+            <BsJustify className='icon'/>
+        </div>
+
+        <div className='header-left'>
+            <BsSearch className='icon'/>
+        </div>  
+
+        <div className='header-right'> 
+            <BsFillBellFill className='icon'/>
+            <BsFillEnvelopeFill className='icon'/>
+            <BsPersonCircle className='icon'/>
+        </div>
+    </header>
   )
 }
 
