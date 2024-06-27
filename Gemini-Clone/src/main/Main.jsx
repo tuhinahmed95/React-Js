ac50e1ea55acfea6ec9920fb67fe98c346483611
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-06-28 04:28:36 +0600

    gemini

--- Gemini-Clone/src/main/Main.jsx
+++ Gemini-Clone/src/main/Main.jsx
@@ -36,6 +36,18 @@ const Main = () => {
                     <img src={assets.code_icon} alt="" />
                 </div>
             </div>
+
+            <div className="main-bottom"> 
+                <div className="search-box"> 
+                    <input type="text" placeholder='Enter a prompt here'/>
+                    <div> 
+                        <img src={assets.gallery_icon} alt="" />
+                        <img src={assets.mic_icon} alt="" />
+                        <img src={assets.send_icon} alt="" />
+                    </div>
+                </div>
+                <p className='bottom-info'>Gemini may display inaccurate info,including about pepole,so doubbl-check ist rspones your privacy gemini app </p>
+            </div>
         </div>
     </div>
   )
