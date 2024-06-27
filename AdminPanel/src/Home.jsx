3ea845916417ebdf6d1f3f2c155f3ea6c34190f4
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-06-27 10:35:28 +0600

    update

--- AdminPanel/src/Home.jsx
+++ AdminPanel/src/Home.jsx
@@ -1,49 +1,47 @@
 import React from 'react'
-import { BsFillArchiveFill, BsFillGrid3X3GapFill,  BsPepoleFill, BsFillBellFill } from 'react-icons/bs'
-
+import { BsFillArchiveFill, BsFillGrid3X3GapFill, BsPepoleFill, BsFillBellFill} from 'react-icons/bs'
 
 
 function Home() {
   return (
-    <main className='main-container'>
-      <div className='main'> 
-        <h3>Dashborad</h3>
+    <main className='main-container'> 
+      <div className="main-title"> 
+        <h3>Dashboard</h3>
       </div>
 
-      <div className='main-card'> 
-        <div className="card"> 
-            <div className='card-inner'> 
-                <h3>PRODUCTS</h3>
-                <BsFillArchiveFill className='card_icon'/>
+      <div className="main-cards"> 
+         <div className="card"> 
+          <div className="card-inner"> 
+              <h3>PRODUCTS</h3>
+              <BsFillArchiveFill className='card-icon'/>
             </div>
-              <h1>300</h1>
+            <h1>300</h1>
           </div>
 
           <div className="card"> 
-            <div className='card-inner'> 
-                <h3>CATEGOTIES</h3>
-                <BsFillGrid3X3GapFill className='card_icon'/>
+            <div className="card-inner"> 
+              <h3>CATEGORIES</h3>
+              <BsFillGrid3X3GapFill className='card-icon'/>
             </div>
-              <h1>12</h1>
+            <h1>12</h1>
           </div>
 
           <div className="card"> 
-            <div className='card-inner'> 
-                <h3>CUSTOMERS</h3>
-                <BsPepoleFill className='card_icon'/>
+            <div className="card-inner"> 
+              <h3>CUSTOMERS</h3>
+              <BsPepoleFill className='card-icon'/>
             </div>
-              <h1>35</h1>
+            <h1>32</h1>
           </div>
 
           <div className="card"> 
-            <div className='card-inner'> 
-                <h3>ALERTS</h3>
-                <BsFillBellFill className='card_icon'/>
+            <div className="card-inner"> 
+              <h3>ALERTS</h3>
+              <BsFillBellFill className='card-icon'/>
             </div>
-              <h1>45</h1>
+            <h1>45</h1>
           </div>
       </div>
-    
 
     </main>
   )
