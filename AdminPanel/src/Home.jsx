62bd91a6d341b346ded71019a370dfb21b768f19
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-06-27 10:56:06 +0600

    update

--- AdminPanel/src/Home.jsx
+++ AdminPanel/src/Home.jsx
@@ -1,8 +1,56 @@
 import React from 'react'
 import { BsFillArchiveFill, BsFillGrid3X3GapFill, BsPepoleFill, BsFillBellFill} from 'react-icons/bs'
+import { BarChart, Bar, Rectangle, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
 
 function Home() {
+
+  const data = [
+    {
+      name: 'Page A',
+      uv: 4000,
+      pv: 2400,
+      amt: 2400,
+    },
+    {
+      name: 'Page B',
+      uv: 3000,
+      pv: 1398,
+      amt: 2210,
+    },
+    {
+      name: 'Page C',
+      uv: 2000,
+      pv: 9800,
+      amt: 2290,
+    },
+    {
+      name: 'Page D',
+      uv: 2780,
+      pv: 3908,
+      amt: 2000,
+    },
+    {
+      name: 'Page E',
+      uv: 1890,
+      pv: 4800,
+      amt: 2181,
+    },
+    {
+      name: 'Page F',
+      uv: 2390,
+      pv: 3800,
+      amt: 2500,
+    },
+    {
+      name: 'Page G',
+      uv: 3490,
+      pv: 4300,
+      amt: 2100,
+    },
+  ];
+  
+
   return (
     <main className='main-container'> 
       <div className="main-title"> 
@@ -13,7 +61,7 @@ function Home() {
          <div className="card"> 
           <div className="card-inner"> 
               <h3>PRODUCTS</h3>
-              <BsFillArchiveFill className='card-icon'/>
+              <BsFillArchiveFill className='card_icon'/>
             </div>
             <h1>300</h1>
           </div>
@@ -21,7 +69,7 @@ function Home() {
           <div className="card"> 
             <div className="card-inner"> 
               <h3>CATEGORIES</h3>
-              <BsFillGrid3X3GapFill className='card-icon'/>
+              <BsFillGrid3X3GapFill className='card_icon'/>
             </div>
             <h1>12</h1>
           </div>
@@ -29,7 +77,7 @@ function Home() {
           <div className="card"> 
             <div className="card-inner"> 
               <h3>CUSTOMERS</h3>
-              <BsPepoleFill className='card-icon'/>
+              <BsPepoleFill className='card_icon'/>
             </div>
             <h1>32</h1>
           </div>
@@ -37,12 +85,37 @@ function Home() {
           <div className="card"> 
             <div className="card-inner"> 
               <h3>ALERTS</h3>
-              <BsFillBellFill className='card-icon'/>
+              <BsFillBellFill className='card_icon'/>
             </div>
             <h1>45</h1>
           </div>
       </div>
 
+      <div className="charts"> 
+      return (
+      <ResponsiveContainer width="100%" height="100%">
+        <BarChart
+          width={500}
+          height={300}
+          data={data}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="pv" fill="#8884d8" activeBar={<Rectangle fill="pink" stroke="blue" />} />
+          <Bar dataKey="uv" fill="#82ca9d" activeBar={<Rectangle fill="gold" stroke="purple" />} />
+        </BarChart>
+      </ResponsiveContainer>
+    
+      </div>
     </main>
   )
 }
