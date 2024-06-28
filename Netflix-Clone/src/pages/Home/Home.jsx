dcbd83effb69b6ccce562e120e1d8b8ddfb2cc53
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-06-29 04:20:55 +0600

    update netflix

--- Netflix-Clone/src/pages/Home/Home.jsx
+++ Netflix-Clone/src/pages/Home/Home.jsx
@@ -3,6 +3,10 @@ import './Home.css'
 import Navbar from '../../components/Navbar/Navbar'
 import hero_banner from '../../assets/hero_banner.jpg'
 import hero_title from '../../assets/hero_title.png'
+import play_icon from '../../assets/play_icon.png'
+import info_icon from '../../assets/info_icon.png'
+import TitleCards from '../../components/TitleCard/TitleCards'
+import Footer from '../../components/Footer/Footer'
 
 const Home = () => {
   return (
@@ -13,8 +17,20 @@ const Home = () => {
         <div className="hero-caption"> 
           <img src={hero_title} alt="" className='caption-img'/>
           <p>Discovering his ties to his secret ancient order,a young man living in modern istanbul inbarks on quest to save the city from an inmortal enemy</p>
+          <div className="hero-btns"> 
+            <button className='btn'><img src={play_icon} alt="" />Play</button>
+            <button className='btn dark-btn'><img src={info_icon} alt="" />More Info</button>
+          </div>
+          <TitleCards />
         </div>
       </div>
+      <div className="more-card"> 
+      <TitleCards title={"Blockbuster Movies"}/>
+      <TitleCards title={"Only on Netflix"}/>
+      <TitleCards title={"Upcomig"}/>
+      <TitleCards title={"Top Pics for You"}/>
+      </div>
+      <Footer />
     </div>
   )
 }
