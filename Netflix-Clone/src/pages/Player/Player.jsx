2dbd6831525a1a84d6919cbb2495fd6a0e8aa21a
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-06-29 08:11:35 +0600

    update

--- Netflix-Clone/src/pages/Player/Player.jsx
+++ Netflix-Clone/src/pages/Player/Player.jsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import './Player.css'
 import back_arrow_icon from '../../assets/back_arrow_icon.png'
-import {useParams} from 'react-router-dom'
+import {useParams, useNavigate} from 'react-router-dom'
 
 const Player = () => {
 
-  const {id} = useParams()
+  const {id} = useParams();
+  const navigate = useNavigate();
 
   const [apiData, setApiData] = useState({ 
     name:"",
@@ -24,7 +25,7 @@ const Player = () => {
 
   useEffect(()=>{ 
 
-    fetch('https://api.themoviedb.org/3/movie/1022789/videos?language=en-US', options)
+    fetch(`https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`, options)
     .then(response => response.json())
     .then(response => setApiData(response.results[0]))
     .catch(err => console.error(err));
@@ -36,7 +37,7 @@ const Player = () => {
 
   return (
     <div className='player'>
-      <img src={back_arrow_icon} alt="" />
+      <img src={back_arrow_icon} alt="" onClick={()=>{navigate(-2)}}/>
       <iframe width='90%' height='90%' src={`http://www.youtube.com/embed/${apiData.key}`} title='tutoria' frameBorder='0' allowFullScreen></iframe>
       <div className="player-info"> 
         <p>{apiData.published_at.slice(0,10)}</p>
