89895887995c24b5ceff78ea404d506543244dd7
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-06-30 21:39:12 +0600

    food delivary

---
+++ Food-delivary/src/Components/Footer/Footer.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import './Footer.css'
+import { assets } from '../../assets/assets'
+const Footer = () => {
+  return (
+    <div className='footer' id='footer'>
+        <div className="footer-content">
+            <div className="footer-content-left">
+                <img src={assets.logo} alt="" />
+                <p>Lorem ipsum, dolor sit amet consectetur 
+                    adipisicing elit. Consequuntur soluta harum 
+                    rerum ratione error nesciunt, quos voluptatibus 
+                    quia labore dolores?
+                </p>
+                <div className="footer-social-icons">
+                    <img src={assets.facebook_icon} alt="" />
+                    <img src={assets.twitter_icon} alt="" />
+                    <img src={assets.linkedin_icon} alt="" />
+                </div>
+            </div>
+
+           
+            <div className="footer-content-center">
+                <h2>COMPANY</h2>
+                <ul>
+                    <li>Home</li>
+                    <li>About us</li>
+                    <li>Delivery</li>
+                    <li>Privacy policy</li>
+                </ul>
+            </div>
+            <div className="footer-content-right">
+                <h2>GET IN TOUCH</h2>
+                <ul>
+                    <li>01918339620</li>
+                    <li>farjana@gmail.com</li>
+                </ul>
+            </div>
+        </div>
+        <hr />
+        <p className="footer-copyright">
+            Copyright by @ Farjana - All Right Reserved   </p>
+      
+    </div>
+  )
+}
+
+export default Footer
---
+++ Food-delivary/src/Context/StoreContext.jsx
@@ -0,0 +1,63 @@
+import { createContext, useEffect, useState } from "react";
+import { food_list } from "../assets/assets";
+export const StoreContext = createContext(null)
+
+const StoreContextProvider = (props) => {
+
+    // for cart item
+    const [cartItems,setCartItems] = useState({});
+    
+    const addToCart = (itemId) => {
+        if (!cartItems[itemId]) {
+            setCartItems((prev) => ({ ...prev, [itemId]: 1 }));
+        } else {
+            setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
+        }
+    };
+    const removeFromCart = (itemId) => {
+        setCartItems(cartItems => {
+            const newItems = { ...cartItems };
+            if (newItems[itemId]) {
+                newItems[itemId]--;
+                if (newItems[itemId] <= 0) {
+                    delete newItems[itemId];
+                }
+            }
+            return newItems;
+        });
+    };
+
+   const getTotalFromCart = (itemId) => {
+    setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
+   }
+   const getTotalCartAmount = () => {
+    let totalAmount = 0;
+    for(const item in cartItems){
+        if (cartItems[item]>0){
+            let itemInfo = food_list.find((product)=>product._id === item)
+        totalAmount += itemInfo.price* cartItems[item];  
+        }
+          
+   }
+   return totalAmount;
+
+}
+
+    const contextValue = {
+        food_list,
+        cartItems,
+        setCartItems,
+        addToCart,
+        removeFromCart,
+        getTotalCartAmount
+
+    }
+
+    return(
+        <StoreContext.Provider value={contextValue}>
+            {props.children}
+        </StoreContext.Provider>
+    )
+}
+
+export default StoreContextProvider;
