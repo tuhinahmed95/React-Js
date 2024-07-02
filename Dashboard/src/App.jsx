de85c50f47be79316f6f04495421688851d70528
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-07-02 11:31:23 +0600

    delete

--- Dashboard/src/App.jsx
+++ Dashboard/src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import ManageDash from './Components/ManageDash';
+import { BrowserRouter, createBrowserRouter } from 'react-router-dom';
+
 
 const App = () => {
   return (
     <div>
-      <ManageDash />
+      <BrowserRouter> 
+        
+      </BrowserRouter>
     </div>
   );
 };
--- Dashboard/src/Components/Footer.jsx
+++ Dashboard/src/Components/Footer.jsx
@@ -2,14 +2,9 @@ import React from 'react';
 
 const Footer = () => {
     return (
-       
-          <footer className="main-footer">
-            <div className="float-right d-none d-sm-block">
-            <b>Version</b> 3.2.0
-            </div>
-            <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
-        </footer>
-    
+        <div>
+            
+        </div>
     );
 };
 
