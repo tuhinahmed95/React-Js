c546e55afef706314d4ea282bc907d4e5ba25f80
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-07-04 09:59:24 +0600

    update

---
+++ Pos/Pos-Project/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Sidebar from './Admin/Components/Sidebar';
+
+const App = () => {
+  return (
+    <div>
+      <Sidebar />
+    </div>
+  );
+};
+
+export default App;
