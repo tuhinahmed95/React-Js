6dc699f067deaf15b3478d25a4cc47dddfcaed7d
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-07-12 19:38:37 +0600

    delete

--- admin/AdminPanel/src/admin/das_components/Sidebar.jsx
+++ admin/AdminPanel/src/admin/das_components/Sidebar.jsx
@@ -11,60 +11,7 @@ const Sidebar = () => {
         <span>Dashboard</span>
       </a>
     </li>{/* End Dashboard Nav */}
-    <li className="nav-item">
-      <a className="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse" href="#">
-        <i className="bi bi-menu-button-wide" /><span>Components</span><i className="bi bi-chevron-down ms-auto" />
-      </a>
-      <ul id="components-nav" className="nav-content collapse " data-bs-parent="#sidebar-nav">
-        <li>
-          <a href="components-alerts.html">
-            <i className="bi bi-circle" /><span>Alerts</span>
-          </a>
-        </li>
-        <li>
-          <a href="components-accordion.html">
-            <i className="bi bi-circle" /><span>Accordion</span>
-          </a>
-        </li>
-        <li>
-          <a href="components-badges.html">
-            <i className="bi bi-circle" /><span>Badges</span>
-          </a>
-        </li>
-        <li>
-          <a href="components-breadcrumbs.html">
-            <i className="bi bi-circle" /><span>Breadcrumbs</span>
-          </a>
-        </li>
-        <li>
-          <a href="components-buttons.html">
-            <i className="bi bi-circle" /><span>Buttons</span>
-          </a>
-        </li>
-        <li>
-          <a href="components-cards.html">
-            <i className="bi bi-circle" /><span>Cards</span>
-          </a>
-        </li>
-       
-        <li>
-          <a href="components-list-group.html">
-            <i className="bi bi-circle" /><span>List group</span>
-          </a>
-        </li>
-        <li>
-          <a href="components-modal.html">
-            <i className="bi bi-circle" /><span>Modal</span>
-          </a>
-        </li>
-        <li>
-          <a href="components-tabs.html">
-            <i className="bi bi-circle" /><span>Tabs</span>
-          </a>
-        </li>
-
-      </ul>
-    </li>{/* End Components Nav */}
+   
     <li className="nav-item">
       <a className="nav-link collapsed" data-bs-target="#forms-nav" data-bs-toggle="collapse" href="#">
         <i className="bi bi-journal-text" /><span>Forms</span><i className="bi bi-chevron-down ms-auto" />
